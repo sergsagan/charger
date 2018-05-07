bb9c6eb136ba227451ee4ede0efbd089f3c17a66
sergey <sergey@judolaunch.com>
2018-05-07 19:01:09 +0300

    create main-screen

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,14 @@ $(function() {
 	
     $("head").append("<link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' />");
 
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        nextArrow: '<a class="icon-left" href="#">prev</a>',
+        prevArrow: '<a class="icon-right" href="#">next</a>'
+    });
+
 });
